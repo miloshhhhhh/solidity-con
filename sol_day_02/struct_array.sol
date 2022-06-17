ce4ffaf0f5a5ffe07e5b13577264e29ba8216b0b
mikectrx <75224808+mikec@users.noreply.github.com>
2022-06-17 23:42:02 +0200

    struct & array

---
+++ sol_day_02/struct_array.sol
@@ -0,0 +1,13 @@
+struct Person {
+  uint age;
+  string name;
+}
+
+Person[] public people;
+
+
+// create a New Person:
+Person satoshi = Person(172, "Satoshi");
+
+// Add that person to the Array:
+people.push(satoshi);
